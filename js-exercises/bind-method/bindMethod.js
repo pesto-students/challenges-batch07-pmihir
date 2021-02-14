18433710a123c4a9f3ef2b6e0d475c27e2693893
Mihir Panchal <pmihir98512@gmail.com>
2021-02-14 13:03:26 +0530

    Day5 Exercise Solution

--- js-exercises/bind-method/bindMethod.js
+++
@@ -1,6 +0,0 @@
-function bind(...args) {
-  const fn = args[0];
-  const obj = args[1];
-  return (val) => fn.call(obj, val);
-}
-module.exports = { bind };
--- js-exercises/sum-even-args/sumEvenArgs.js
+++
@@ -1,3 +0,0 @@
-const sumEvenArgs = (...args) => args.reduce((sum, val) => ((val % 2 === 0) ? sum + val : sum), 0);
-
-export { sumEvenArgs };
